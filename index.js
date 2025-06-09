32343a40048df68215baa4d89c32c84c4bdde2b6
Cléver Guimarães <clever.filho@gmail.com>
2025-06-09 10:42:37 -0300

    Ajustes do Teste
    Ajustes feito para corrigir as falhas propositais.

--- index.js
+++ index.js
@@ -1,59 +1,95 @@
-const puppeteer = require('puppeteer-extra')
-const pluginStealth = require('puppeteer-extra-plugin-stealth')
-puppeteer.use(pluginStealth())
+const puppeteer = require("puppeteer-extra");
+const pluginStealth = require("puppeteer-extra-plugin-stealth");
+puppeteer.use(pluginStealth());
 
-async function sleep (seconds) {
+async function sleep(seconds) {
   return await new Promise((resolve) => {
-    setTimeout(() => resolve({}), seconds * 1000)
-  })
+    setTimeout(() => resolve({}), seconds * 1000);
+  });
 }
 
-async function search (frame, cnpj) {
-  await sleep(1 + Math.random() * 1)
-  await frame.type('input[id=cnpj]', cnpj)
-  await sleep(1 + Math.random() * 1)
-  await frame.click('button[confirmar]')
-}
-
-async function getData (frame) {
-  const cnpj = await frame.evaluate(() => {
-    const elementFrame = document.querySelector("#conteudo > div:nth-child(0) > div.panel-body > span:nth-child(1)")
-    return elementFrame.textContent
-  })
-  const situacaoSN = ''
-  const situacaoSimei = ''
-
-  return {
-    cnpj,
-    situacaoSN,
-    situacaoSimei
+async function search(frame, cnpj) {
+  try {
+    await frame.waitForSelector("input[id=Cnpj]", { visible: true });
+    await sleep(1 + Math.random() * 1);
+    await frame.type("input[id=Cnpj]", cnpj);
+    await sleep(1 + Math.random() * 1);
+    await frame.click("#consultarForm > button");
+  } catch (err) {
+    console.error("Erro na função search:", err);
+    throw err;
   }
 }
 
-async function main () {
-  const cnpj = '25021140000177'
-  const browser = await puppeteer.launch({
-    executablePath: '',
-    headless: false,
-    ignoreHTTPSErrors: true,
-    args: [
-      '--no-sandbox',
-      '--disable-web-security',
-      '--disable-features=IsolateOrigins,site-per-process',
-      '--start-maximized'
-    ]
-  })
+async function getData(frame) {
+  try {
+    await frame.waitForSelector("#conteudo");
+    await sleep(1 + Math.random() * 1);
+    const cnpj = await frame.evaluate(() => {
+      const elementFrame = document.querySelector(
+        "#conteudo > div:nth-child(2) > div.panel-body > span:nth-child(1)"
+      );
+      return elementFrame?.textContent || "";
+    });
+    const situacaoSN = await frame.evaluate(() => {
+      const elementFrame = document.querySelector(
+        "#conteudo > div:nth-child(3) > div.panel-body > span:nth-child(1)"
+      );
+      return elementFrame?.textContent || "";
+    });
+    const situacaoSimei = await frame.evaluate(() => {
+      const elementFrame = document.querySelector(
+        "#conteudo > div:nth-child(3) > div.panel-body > span:nth-child(3)"
+      );
+      return elementFrame?.textContent || "";
+    });
 
-  const pages = await browser.pages()
-  const page = pages[0]
-  await page.goto('https://www8.receita.fazenda.gov.br/SimplesNacional/aplicacoes.aspx?id=21')
-  await page.waitForSelector('#frame')
-  const elementFrame = await page.$('#frame')
-  const frame = await elementFrame.contentFrame()
-  await search(frame, cnpj)
-  const data = await getData(frame)
-  console.log(data)
-  await browser.close()
+    return {
+      cnpj,
+      situacaoSN,
+      situacaoSimei,
+    };
+  } catch (err) {
+    console.error("Erro na função getData", err);
+    throw err;
+  }
 }
 
-main()
+async function main() {
+  const cnpj = "25021140000177";
+  let browser;
+
+  try {
+    browser = await puppeteer.launch({
+      executablePath: puppeteer.executablePath(),
+      headless: false,
+      ignoreHTTPSErrors: true,
+      args: [
+        "--no-sandbox",
+        "--disable-web-security",
+        "--disable-features=IsolateOrigins,site-per-process",
+        "--start-maximized",
+      ],
+    });
+
+    const pages = await browser.pages();
+    const page = pages[0];
+    await page.goto(
+      "https://www8.receita.fazenda.gov.br/SimplesNacional/aplicacoes.aspx?id=21"
+    );
+    await page.waitForSelector("#frame");
+    const elementFrame = await page.$("#frame");
+    const frame = await elementFrame.contentFrame();
+    await search(frame, cnpj);
+    const data = await getData(frame);
+    console.log(data);
+  } catch (err) {
+    console.error("Erro na função main: ", err);
+  } finally {
+    if (browser) {
+      await browser.close();
+    }
+  }
+}
+
+main();
